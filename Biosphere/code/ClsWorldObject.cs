cda652e1e7dfed0e8156f0d53b252943e2ae0c2b
cfward <camiloward@gmail.com>
2015-06-07 21:47:26 -0400

    firsr commit

---
+++ Biosphere/code/ClsWorldObject.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biosphere;
+
+namespace Biosphere.code
+{
+    class ClsWorldObject<T>
+    {
+        public int BaseCost { get; set; }
+        public double CostMultiplier { get; set; }
+        public int CurrentCost { get; set; }
+        public int NumberBought { get; set; }
+        public int NumberOwned { get; set; }
+        public int BioMass { get; set; }
+        public int Hunger { get; set; }
+        public T ObjectType { get; set; }
+        public double ChanceToEvolve { get; set; }
+        public int IncomeIncrease { get; set; }
+
+        public ClsWorldObject(int p_dblBaseCost, double p_dblCostMultiplier, int p_intBioMass, double p_dblEvolveChance, int p_intIncomeIncrease, T p_objType)
+        {
+            BaseCost = p_dblBaseCost;
+            CostMultiplier = p_dblCostMultiplier;
+            BioMass = p_intBioMass;
+            NumberBought = 0;
+            NumberOwned = 0;
+            CurrentCost = BaseCost;
+            ObjectType = p_objType;
+            ChanceToEvolve = p_dblEvolveChance;
+            IncomeIncrease = p_intIncomeIncrease;
+            Hunger = BioMass / 10;
+        }
+
+        public bool CanReproduce(int p_intNumberOfFood)
+        {
+            return NumberOwned * BioMass / 10 < p_intNumberOfFood;            
+        }
+       
+        public int BuyObject()
+        {
+            CalculateCost();
+            NumberBought++;
+            NumberOwned++;
+
+            return IncomeIncrease;
+        }
+
+        public int IncrementObject(int p_intNumberToIncrement = 1)
+        {
+            NumberOwned += p_intNumberToIncrement;
+
+            return IncomeIncrease * p_intNumberToIncrement;
+        }
+
+        private void CalculateCost()
+        {
+            int intCost = 0;
+
+            for (int index = 0; index < NumberBought; index++)
+            {
+                intCost += (int)Math.Truncate(BaseCost * NumberBought * CostMultiplier);
+            }
+
+            CurrentCost = BaseCost + intCost;
+        }
+
+        public int ConsumeObject(int p_intAmount)
+        {
+            if (NumberOwned >= p_intAmount)
+            {
+                NumberOwned -= p_intAmount;
+
+                if (NumberOwned < NumberBought)
+                {
+                    NumberBought = NumberOwned;
+                }
+
+                CalculateCost();
+            }
+            else
+            {
+                p_intAmount = 0;
+            }
+
+            return IncomeIncrease * p_intAmount * -1;
+        }
+    }
+}
---
+++ Biosphere/constants/Constants.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biosphere
+{
+    public class Constants
+    {
+        public const int STARTING_SUN = 10;
+        public const int STARTING_INCOME = 10;      
+
+        public const int STARTING_CARBON_COST = 10;
+        public const double STARTING_CARBON_MULTIPLIER = .1;
+        public const int STARTING_CARBON_BIOMASS = 10;
+        public const double STARTING_CARBON_EVOLVE_CHANCE = .1;
+        public const int STARTING_CARBON_INCOME = 1;
+
+        public const int STARTING_OXYGEN_COST = 10;
+        public const double STARTING_OXYGEN_MULTIPLIER = .1;
+        public const int STARTING_OXYGEN_BIOMASS = 10;
+        public const double STARTING_OXYGEN_EVOLVE_CHANCE = .1;
+        public const int STARTING_OXYGEN_INCOME = 1;
+
+        public const int STARTING_HYDROGEN_COST = 10;
+        public const double STARTING_HYDROGEN_MULTIPLIER = .1;
+        public const int STARTING_HYDROGEN_BIOMASS = 10;
+        public const double STARTING_HYDROGEN_EVOLVE_CHANCE = .1;
+        public const int STARTING_HYDROGEN_INCOME = 1;
+
+        public const int STARTING_CO2_COST = 100;
+        public const double STARTING_CO2_MULTIPLIER = .2;
+        public const int STARTING_CO2_BIOMASS = 50;
+        public const double STARTING_CO2_EVOLVE_CHANCE = .1;
+        public const int STARTING_CO2_INCOME = 15;
+
+        public const int STARTING_O2_COST = 100;
+        public const double STARTING_O2_MULTIPLIER = .2;
+        public const int STARTING_O2_BIOMASS = 50;
+        public const double STARTING_O2_EVOLVE_CHANCE = .1;
+        public const int STARTING_O2_INCOME = 15;
+
+        public const int STARTING_O3_COST = 1000;
+        public const double STARTING_O3_MULTIPLIER = .2;
+        public const int STARTING_O3_BIOMASS = 10;
+        public const double STARTING_O3_EVOLVE_CHANCE = .1;
+        public const int STARTING_O3_INCOME = 1;
+
+        public const int STARTING_H2O_COST = 100;
+        public const double STARTING_H2O_MULTIPLIER = .2;
+        public const int STARTING_H2O_BIOMASS = 50;
+        public const double STARTING_H2O_EVOLVE_CHANCE = .1;
+        public const int STARTING_H2O_INCOME = 15;
+
+        public const int STARTING_ALGAE_COST = 100;
+        public const double STARTING_ALGAE_MULTIPLIER = .2;
+        public const int STARTING_ALGAE_BIOMASS = 100;
+        public const double STARTING_ALGAE_EVOLVE_CHANCE = .1;
+        public const int STARTING_ALGAE_INCOME = 60;
+
+        public const int STARTING_SMALL_SEA_COST = 1000;
+        public const double STARTING_SMALL_SEA_MULTIPLIER = .2;
+        public const int STARTING_SMALL_SEA_BIOMASS = 250;
+        public const double STARTING_SMALL_SEA_EVOLVE_CHANCE = .1;
+        public const int STARTING_SMALL_SEA_INCOME = 150;
+
+        public const int STARTING_OXYGEN_FACTORY_COST = 1000;
+        public const double STARTING_OXYGEN_FACTORY_MULTIPLIER = .2;
+        public const int STARTING_OXYGEN_FACTORY_BIOMASS = 500;
+        public const double STARTING_OXYGEN_FACTORY_EVOLVE_CHANCE = .1;
+        public const int STARTING_OXYGEN_FACTORY_INCOME = 10;
+
+        public const int STARTING_CARBON_FACTORY_COST = 1000;
+        public const double STARTING_CARBON_FACTORY_MULTIPLIER = .2;
+        public const int STARTING_CARBON_FACTORY_BIOMASS = 500;
+        public const double STARTING_CARBON_FACTORY_EVOLVE_CHANCE = .1;
+        public const int STARTING_CARBON_FACTORY_INCOME = 10;
+
+        public const int STARTING_HYDROGEN_FACTORY_COST = 1000;
+        public const double STARTING_HYDROGEN_FACTORY_MULTIPLIER = .2;
+        public const int STARTING_HYDROGEN_FACTORY_BIOMASS = 500;
+        public const double STARTING_HYDROGEN_FACTORY_EVOLVE_CHANCE = .1;
+        public const int STARTING_HYDROGEN_FACTORY_INCOME = 10;
+
+    }
+
+    public enum WorldObjectType
+    {
+        Oxygen,
+        Hydrogen,
+        Carbon,
+        CarbonDioxide,
+        OxygenMolecule,
+        Ozone,
+        Water,
+        Algae,
+        SmallSeaCreature,
+        CarbonFactory,
+        HydrogenFactory,
+        OxygenFactory
+    }
+}
